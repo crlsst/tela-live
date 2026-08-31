e49659a64b9e4400e70a957353da77c6f8b05d90
crlsst <carloshenriquefontinele4@gmail.com>
2026-08-31 18:54:21 -0300

    server.js

--- server.js
+++ server.js
@@ -69,4 +69,7 @@ wss.on("connection",ws=>{
   if(!r.clients.size)rooms.delete(ws.roomId);
  });
 });
-server.listen(process.env.PORT||3000,()=>console.log("http://localhost:3000"));
+const PORT = process.env.PORT || 10000;
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Tela Live rodando na porta ${PORT}`);
+});
